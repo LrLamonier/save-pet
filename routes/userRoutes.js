3a3b15a493981b32a34fcfeaf4e2ab3758266f63
Vanderlândio Z. da Rocha <vandodev@gmail.com>
2022-09-08 20:51:13 -0300

    Conexão com o banco feita com sucesso. Criado a model da tabela usuarioPessoa

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
+const UsuarioTeste = require("../controllers/UsuarioTeste");
 
 const router = express.Router();
 
@@ -21,5 +22,13 @@ router.get("/profile", authController.protect, userController.getMe);
 
 // editar perfil
 router.patch("/profile", authController.protect, userController.editProfile);
+
+//Rota de teste do banco Vando
+// router.get('teste', (req, res) =>{
+//     res.render()
+// })
+router.post('/teste-banco', async (req, res, next) =>{
+    await UsuarioTeste.create(req, res, next)
+})
 
 module.exports = router;
