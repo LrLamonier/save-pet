cab53437190b29316bee7022f8884edfeca6925c
Gisele <gisaamuniz@hotmail.com>
2022-09-23 14:55:04 -0300

    iniciando testes unitarios

---
+++ __tests__/routes.t.js
@@ -0,0 +1,31 @@
+
+import request from 'supertest';
+import express from 'express';
+import router from '../routes/main.js';
+
+const app = new express();
+app.use('/', router);
+
+describe('Adopt Controller Routes', function () {
+
+  test('responds to /', async () => {
+    const res = await request(app).get('/');
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+  });
+  /*
+  test('responds to /hello/:name', async () => {
+    const res = await request(app).get('/hello/jaxnode'); 
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toEqual('hello jaxnode!');
+  });
+
+  test('responds to /hello/Annie', async () => {
+    const res = await request(app).get('/hello/Annie'); 
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toEqual('hello Annie!');
+  });
+*/
+});
